392e5b9d0287fa126aa1713afa3b9aaf38bdd6cd
unknown <uly061193@mail.ru>
2016-02-05 16:14:34 +0300

    Cool

--- 1lesson.R
+++ 1lesson.R
@@ -1,3 +1,24 @@
-d=1:100
-f=sin(d)
-plot(d,f)
+t=c(T,TRUE)
+f=c(F,FALSE)
+# commentary as numeric log()
+?as `logical-class`()
+?as logi
+
+# tip peremens
+# func (a,b,c)
+plot(density(rnorm(1:100)),col="blue")
+# operator (=)
+t=25^.5
+# log operatory "==" logical oper
+
+z==4
+z>4=4
+z<4=4
+z=c(1,4,10)
+# seqi(from=
+)
+A=c("A","B","C")
+B=1:5
+C=c(T,F)
+#names func
+#f
